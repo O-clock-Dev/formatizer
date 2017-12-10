af132e3ca8869ac5d5219a4c0b1831d64f28a3ba
Maxime Vasse <mallaxoz@gmail.com>
2017-12-10 22:35:19 +0100

    emoji with spaces?

--- src/components/Piece/Emoji/style.js
+++ src/components/Piece/Emoji/style.js
@@ -11,6 +11,5 @@ import Glamorous from 'glamorous';
  * Style
  */
 export const Style = Glamorous.span({
-  display: 'inline',
   '& .emojione': { display: 'inline', height: '1.2em', width: '1.2em' },
 });
