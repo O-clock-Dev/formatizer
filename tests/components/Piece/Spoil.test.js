d4d37363c2600ad967256189b2986a9b61cffd6e
Alexandre Bourdeaud'hui <alexandre.bourdeaudhui@gmail.com>
2018-03-28 23:25:38 +0200

    Add tests for Spoil

---
+++ tests/components/Piece/Spoil.test.js
@@ -0,0 +1,84 @@
+/*
+ * Package Import
+ */
+import React from 'react';
+import { should } from 'chai';
+import { mount } from 'enzyme';
+
+/*
+ * Local Import
+ */
+import { Formatizer } from 'src';
+import Format from 'src/components/Format';
+import Spoil from 'src/components/Piece/Spoil';
+import Highlight from 'src/components/Piece/Highlight';
+import { Summary } from 'src/components/Piece/Spoil/style';
+
+/*
+ * Code
+ */
+should();
+
+/*
+ * Tests
+ */
+describe('** src/components/Piece/Spoil.js **', () => {
+  it('should add <Spoil />', () => {
+    const message = 'Bonjour, ###je suis un spoiler###';
+    const wrapper = mount(<Formatizer>{message}</Formatizer>);
+    const component = wrapper.find(Spoil);
+
+    // Should have <Spoil />
+    component.should.have.length(1);
+
+    // Should have `Summary` with text `Spoiler`
+    component
+      .find(Summary)
+      .props()
+      .should.have.property('children')
+      .which.be.equal('Spoiler');
+
+    // Should have text `Je suis un spoiler`
+    component
+      .find(Format)
+      .props()
+      .should.have.property('children')
+      .which.be.equal('je suis un spoiler');
+  });
+
+  it('should manage many Spoils and newlines', () => {
+    const message = `Bonjour,
+      Lorem ipsum
+      ### coucou ### !
+      ###
+      second spoiler
+      ###
+    `;
+    const wrapper = mount(<Formatizer>{message}</Formatizer>);
+    wrapper.find(Spoil).should.have.length(2);
+  });
+
+  it('should correctly display a <Spoil /> with space before ###', () => {
+    const message = '###\n  coucou###';
+    const wrapper = mount(<Formatizer>{message}</Formatizer>);
+    const component = wrapper.find(Spoil);
+
+    component
+      .find(Format)
+      .props()
+      .should.have.property('children')
+      .which.be.equal('  coucou');
+  });
+
+  it('should add <Highlight /> in <Spoil />', () => {
+    const message = '### ```js const a = "je suis un snippet"; ``` ###';
+    const wrapper = mount(<Formatizer>{message}</Formatizer>);
+    const component = wrapper.find(Spoil);
+
+    // Should have <Spoil />
+    component.should.have.length(1);
+
+    // Should have <Highlight />
+    component.find(Highlight).should.have.length(1);
+  });
+});
