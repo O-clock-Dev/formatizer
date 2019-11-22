633c343c3719a0ef56c9dff3405218fb31576b01
Alexandre Bourdeaud'hui <alexandre.bourdeaudhui@gmail.com>
2019-11-22 02:00:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ export { default as Emojimart } from 'emoji-mart';
 export { default as Formatizer } from 'src/components/Formatizer';
 export { default as Picker } from 'src/components/Picker';
 export { default as Emoji } from 'src/components/Piece/Emoji';
-export { default as setImagePath } from 'src/config';
